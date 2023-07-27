c525dceb9142eeacec18d6ddae5b17271790afba
jGar6z <garces.neto2020@gmail.com>
2023-07-27 11:20:00 -0300

    27/07/23

---
+++ script.js
@@ -0,0 +1,35 @@
+//Responsive Header
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+//Animals Slider
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+    nextImage();
+}, 8000)
+
+function nextImage(){
+    count++;
+    if (count > 4){
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
+}
